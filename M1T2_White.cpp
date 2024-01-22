23b32614e24a5215faaea62a8844960540a9eca0
whiteb6633 <157392861+whiteb6633@users.noreply.github.com>
2024-01-22 16:24:40 +0000

    Revised M1T2 assignment

---
+++ M1T2_White.cpp
@@ -0,0 +1,23 @@
+// CSC 134
+// M1T2
+// Brandon White
+// 01/22/2024
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Hello World!" << endl;
+    cout << "My name is Brandon White!" << endl;
+    cout << "I want to be an engineer." << endl;
+
+    // Ask a question
+    string name;
+    cout << "What's your name? ";
+    cin >> name;
+
+    cout << "Nice to meet you, " << name << endl;
+
+cout << endl << endl;
+return 0;
+}
