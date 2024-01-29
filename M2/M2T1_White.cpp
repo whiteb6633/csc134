4409cf92ba4c7438a12cd2b73a61bd2456f4a57b
whiteb6633 <157392861+whiteb6633@users.noreply.github.com>
2024-01-29 16:16:08 +0000

    M2T1 - Receipt with wrong total.

---
+++ M2/M2T1_White.cpp
@@ -0,0 +1,33 @@
+/*
+CSC 134
+M2T1 - Receipt
+Brandon White
+1/29/24
+
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    cout << "M2T1" << endl;
+    // set up variables
+    string meal = "Value Meal";
+    // all in $, except the percent
+    double meal_price = 5.99;
+    double tax_percent = 0.08;
+    double tax_amount = 0;
+    double total = 0;
+
+    // do calculations
+
+    // print the receipt
+    // TODO: fix decimals
+    cout << meal    << "\t$" << meal_price << endl;
+    cout << "Tax:"    <<  "\t\t$" << tax_amount << endl;
+    cout << "Total:"  <<  "\t\t$" << total << endl;
+
+    return 0;
+}
