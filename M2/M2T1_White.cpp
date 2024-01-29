cea4d3fe9d7b5ef20b74c49f930f72045afa7b3a
whiteb6633 <157392861+whiteb6633@users.noreply.github.com>
2024-01-29 16:38:05 +0000

    M2T1 - Finished

--- M2/M2T1_White.cpp
+++ M2/M2T1_White.cpp
@@ -8,11 +8,14 @@ Brandon White
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 int main ()
 {
     cout << "M2T1" << endl;
+    cout << "Thank you for dining with us" << endl;
+    cout << "----------------------" << endl;
     // set up variables
     string meal = "Value Meal";
     // all in $, except the percent
@@ -20,12 +23,18 @@ int main ()
     double tax_percent = 0.08;
     double tax_amount = 0;
     double total = 0;
+
     // do calculations
+    tax_amount = meal_price * tax_percent;
+    total = meal_price + tax_amount;
 
     // print the receipt
-    // TODO: fix decimals
+    // print this once to set the decimals to exactly 2
+    cout << fixed << setprecision(2);
+
     cout << meal    << "\t$" << meal_price << endl;
     cout << "Tax:"    <<  "\t\t$" << tax_amount << endl;
+    cout << "----------------------" << endl;
     cout << "Total:"  <<  "\t\t$" << total << endl;
 
     return 0;
