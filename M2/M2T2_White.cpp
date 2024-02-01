e7c266864495333b601f794f5616d340bda809df
whiteb6633 <157392861+whiteb6633@users.noreply.github.com>
2024-02-01 18:24:23 +0000

    Revised M2T2, needs cleaning

--- M2/M2T2_White.cpp
+++ M2/M2T2_White.cpp
@@ -6,6 +6,7 @@ Brandon White
 */
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -15,19 +16,35 @@ int main()
     // set up variables (nouns) with cin instead of hardcoding their values
     // Declare variables
 
-    string name;
+    string response;
     int apples;
     double price_each = 0.25;
+    double tax_percent = 0.10;
+    double total = 0;
     
     // Ask for information
     cout << "Hi, what's your name? ";
-    cin >> name;
+    cin >> response;
     cout << "Welcome to our orchard. How many apples would you like? " << endl;
     cin >> apples;
+    cout << fixed << setprecision(2);
     double total_price = apples * price_each;
+
     cout << "Each apple has a cost of $" << price_each << " each." << endl;
     cout << "Your total would be $" << total_price <<  ". Would you like to proceed with this purchase? " << endl;
-    cin >> name;
-   
+    cin >> response;
+    cout << "Great! Here is your receipt." << endl;
+    cout << "" << endl;
+
+    double tax_amount = total_price * tax_percent;
+    double grand_total = total_price + tax_amount;
+    
+    cout << "Thank you for shopping with us." << endl;
+    cout << "------------------------------" << endl;
+    cout << apples << " Apples" << "\t$" << total_price << endl;
+    cout << "Tax:" <<  "\t\t" << tax_amount << endl;
+    cout << "------------------------------" << endl;
+    cout << "Total:" << "\t\t$" << grand_total << endl;
+      
     return 0;
 }
