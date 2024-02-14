6c90804e7f7f015e4b39ceac14ce983bf419c997
whiteb6633 <157392861+whiteb6633@users.noreply.github.com>
2024-02-14 15:31:57 +0000

    starter format

---
+++ M1/starter.cpp
@@ -0,0 +1,13 @@
+// use as starter for any program
+// CSC 134
+// ASSIGNMENT
+// NAME
+// DATE
+
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+    return 0;
+}
---
+++ M3/M3T1_White.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+/*
+CSC134
+M3T1 - Rectangles
+Brandon White
+0/14/2024
+*/
