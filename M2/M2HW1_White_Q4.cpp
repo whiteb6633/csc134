ad54e09219228c513c84a0340b1ca5c6c1072e6c
whiteb6633 <157392861+whiteb6633@users.noreply.github.com>
2024-02-16 03:57:34 +0000

    M2HW1_White_Q4 Completed

---
+++ M2/M2HW1_White_Q4.cpp
@@ -0,0 +1,26 @@
+/*
+CSC 134
+M2HW1 - Gold
+Brandon White
+02/15/2024
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string letsGo, school, team, cheerOne, cheerTwo;
+    letsGo = "Let's go";
+    school = "FTCC";
+    team = "Trojans";
+    cheerOne = letsGo + " " + school;
+    cheerTwo = letsGo + " " + team;
+
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerTwo << endl;
+
+    return 0;
+}
