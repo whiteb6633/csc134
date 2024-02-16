676224862456727a871dfcf5b822e85243e2d89b
whiteb6633 <157392861+whiteb6633@users.noreply.github.com>
2024-02-16 03:29:03 +0000

    M2HW1 Complete

--- M2/M2HW1_WhiteQ3.cpp
+++ M2/M2HW1_WhiteQ3.cpp
@@ -13,7 +13,8 @@ int main ()
     int slices;
     int guests; 
     int remaining;
-
+    int slice_per_guest;
+    
     cout << "Question 3: " << endl;
     cout << "How many pizza's do you want to order?" << endl;
     cin >> pizza;
@@ -22,6 +23,9 @@ int main ()
     cout << "How many visitors are coming?" << endl;
     cin >> guests;
     
+    slice_per_guest = guests * 3;
+    remaining = pizza * slices - slice_per_guest;
+
     cout << "You will have " << remaining << " slices left!" << endl;
 
     return 0;
