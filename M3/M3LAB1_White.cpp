6746eb9c4cf4f566a8cb788c402a9be883db2de0
whiteb6633 <157392861+whiteb6633@users.noreply.github.com>
2024-02-19 16:45:28 +0000

    M3LAB1 Phase A

--- M3/M3LAB1_White.cpp
+++ M3/M3LAB1_White.cpp
@@ -16,19 +16,31 @@ int main() {
 
   if (1 == choice) {
   	cout << "Then you are an amazing person!" << endl;
+    cout << endl; 
     cout << "Did you side with the stormcloaks or the imperials?" << endl;
-    cout << "Type 1 for stormcloaks, Type 2 for imperials:" << endl;
+    cout << "Type 1 for stormcloaks, Type 2 for imperials: ";
     cin >> choice;
-    
+  if (1 == choice) 
+    cout << "Its the right thing to do." << endl;
+  else if (2 == choice)
+    cout << "You should be ashamed of yourself!" << endl;
   }
   else if (2 == choice) {
   	cout << "You're a monster!" << endl;
+    cout << endl;
+    cout << "Do you like RPGs?" << endl;
+    cout << "Type 1 for yes, Type 2 for no: ";
+    cin >> choice;
+  if (1 == choice)
+    cout << "Ok, we can work with this." << endl;
+  else if (2 == choice)
+    cout << "Strike two my friend!" << endl; 
   }
   else {
   	cout << "I'm sorry, that is not a valid choice." << endl;
   }
-
-  cout << "Thanks for playing!" << endl; 
+  cout << endl;
+  cout << "Wait! Don't leave!" << endl; 
   return 0; 
 
 } 
