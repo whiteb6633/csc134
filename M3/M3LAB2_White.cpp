8a78e5b7e6642f8a138e939720472be028e2dda3
whiteb6633 <157392861+whiteb6633@users.noreply.github.com>
2024-02-26 16:30:16 +0000

    M2LAB_White FInished

---
+++ M3/M3LAB2_White.cpp
@@ -0,0 +1,42 @@
+// CSC 134
+// M3LAB2
+// Brandon White
+// 02/26/2024
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+int numerical;
+
+    cout << "What is your numerical grade? ";
+    cin >> numerical;
+    cout << endl;
+    if (numerical >= 90 && numerical <= 100)
+    {
+        cout << "Your letter grade is A";
+    }
+    else if (numerical >= 80 && numerical <= 89)
+    {
+        cout << "Your letter grade is B";
+    }
+    else if (numerical >= 70 && numerical <= 79)
+    {
+        cout << "Your letter grade is C";
+    }
+    else if (numerical >= 60 && numerical <= 69)
+    {
+        cout << "Your letter grade is D";
+    }
+    else if (numerical >= 0 && numerical <=59)
+    {
+        cout << "Your letter grade is F";
+    }
+    if (numerical > 100 || numerical < 0)
+    {
+        cout << "Please enter a value between zero and one-hundred";
+    }
+    return 0;
+}
