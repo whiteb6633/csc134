4757ffec1cfcf4e79fd27e8e496975c77a3cc91a
whiteb6633 <157392861+whiteb6633@users.noreply.github.com>
2024-02-27 18:46:32 +0000

    M3HW Q1 and Q2 Complete

--- M3/M3HW1_White.cpp
+++ M3/M3HW1_White.cpp
@@ -4,6 +4,7 @@
 // 02/26/2024
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
@@ -27,5 +28,44 @@ int main() {
     else {
         cout << "If you're not sure… that's OK." << endl;
     }
+    cout << endl;
+
+    // Question 2
+
+    double price;
+    double tax_percent = 0.08;
+    double tax_amount = 0;
+    double total = 0;
+    double tip = 0.15;
+
+    cout << "Question 2" << endl;
+    cout << "What was the price of the meal? ";
+    cin >> price;
+
+    cout << "Please press 1 for dine-in or 2 for take-away. ";
+    string choice;
+    cin >> choice;
+    cout << endl;
+
+    tax_amount = price * tax_percent;
+    tip = price * 0.15;
+    total = price + tax_amount + tip;
+    cout << fixed << setprecision(2);
+    // Does Tip need to show on reciept for option 2?
+    if (choice == "1") {
+    cout << "Meal:"    << "\t\t$" << price << endl;
+    cout << "Tax:"    <<  "\t\t$" << tax_amount << endl;
+    cout << "Tip:"        "\t\t$" << tip << endl;
+    cout << "----------------------" << endl;
+    cout << "Total:"  <<  "\t\t$" << total << endl;
+    }
+    else if (choice == "2") {
+    total = price + tax_amount;
+    cout << "Meal"    <<  "\t\t$" << price << endl;
+    cout << "Tax:"    <<  "\t\t$" << tax_amount << endl;
+    cout << "----------------------" << endl;
+    cout << "Total:"  <<  "\t\t$" << total << endl;
+    }
+
     return 0;
 }
