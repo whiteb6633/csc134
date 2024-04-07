96f824e30f0685cfef85ed21df7617e050d26ca0
whiteb6633 <157392861+whiteb6633@users.noreply.github.com>
2024-04-07 03:34:45 +0000

    M5HW Q5 needs work

--- M5/M5HW_White.cpp
+++ M5/M5HW_White.cpp
@@ -223,15 +223,20 @@ void Q5() {
     int speed;
     int time;
     int distance;
+
     cout << "What is the speed of the vehicle in MPH? ";
     cin >> speed;
     cout << "How many hours has it traveled? ";
     cin >> time;
+
     distance = speed * time;
+    
     cout << "Hour(s)" << "\t\t" << "Distance Traveled" << endl;
     cout << "____________________________" << endl;
-    cout << time << "\t\t\t" << distance << endl;
-    time++; 
+    while (time <= distance) {
+    cout << time << "\t\t\t" << speed * time << endl;
+    time++;
+    }
 }
     
 
