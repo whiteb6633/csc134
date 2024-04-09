cecb36593f0c558909b3fc807d8967b8d979c95f
whiteb6633 <157392861+whiteb6633@users.noreply.github.com>
2024-04-09 02:11:43 +0000

    M6T1 started

---
+++ M6/M6T1_White.cpp
@@ -0,0 +1,12 @@
+// CSC 134
+// M6T1 - Counting Cars
+// WhiteB
+// 4/8/2024
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Good Luck Friend!" << endl;
+    return 0;
+}
