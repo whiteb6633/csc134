9657b32d9a87eeee5b66be01d81154c83cca3a11
whiteb6633 <157392861+whiteb6633@users.noreply.github.com>
2024-04-10 14:14:23 +0000

    M6T1 work in progress

--- M6/M6T1_White.cpp
+++ M6/M6T1_White.cpp
@@ -6,7 +6,28 @@
 #include <iostream>
 using namespace std;
 
+void P1();
+void P2();
+
 int main () {
-    cout << "Good Luck Friend!" << endl;
+    int options;
+    cout << "Please choose from the following options: " << endl;
+    cout << "P1: [1]" << endl;
+    cout << "P2: [2]" << endl;
+    cin >> option;
+    if (option == 1) {
+        P1();
+    } 
+    if (option == 2) {
+        P2();
+    }
     return 0;
+}
+
+int P1() {
+    cout << "Hello" << endl;
+}
+
+int P2() {
+    cout << "Goodbye" << endl;
 }
